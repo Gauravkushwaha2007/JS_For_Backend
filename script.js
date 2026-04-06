51770d558f262b874a7b640250f33f186eba65c5
Gauravkushwaha2007 <gaurav121212kush@gmail.com>
2026-04-06 20:50:35 +0530

    For each loop created

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('Haa');
+// For Each loop 
+// Use for interact with each element in array without creating duplicate of losing of data
+let arr = [12,23,34,45,56];
+
+arr.forEach((val)=>{
+    console.log(val+ " Gaurav");
+});
+
+
+
