a4ee0c7211bbcef212388f87c49d9a8131d4ec8d
Gauravkushwaha2007 <gaurav121212kush@gmail.com>
2026-04-06 21:02:38 +0530

    Added Filter and Map method

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ arr.forEach((val)=>{
 
 
 
+
+
+// map* metohd 
+// use where we need to create a same size of new array with value(can be differe or same)
+let ans = arr.map((val)=>{
+    return val+1;
+})
+ console.log(ans);
+
+
+
+
+
+//filter 
+//use where we want a new array but with filtered value, but array size will not be same, where map method returns same size of array
+let newAns = arr.filter(function(val){
+    if(val>25) return val;
+})
+
+console.log(newAns);
