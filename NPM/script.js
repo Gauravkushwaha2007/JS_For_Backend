51963ff4aedfa26fbc0fceb4e6d0c57f82aa4e85
Gauravkushwaha2007 <gaurav121212kush@gmail.com>
2026-04-10 15:55:05 +0530

    Express Learning

--- NPM/script.js
+++ NPM/script.js
@@ -1,3 +1,15 @@
 const fs = require('fs');
 
-fs.writeFile("Message.txt", "This is Gaurav from Backend side");
+fs.writeFile("Message.txt", "This is Gaurav from Backend side",((error)=>{
+    if(error){
+        console.log("File couldn't create");
+    }
+    else{
+        console.log("Done");
+    }
+}));
+
+fs.rename('./Message.txt', 'msg.txt',((error)=>{if(error){console.log(error)}}));
+
+fs.rm('./Msg.txt',{recursive: true, force: true}, ((e)=>{console.log(e)}))
+
